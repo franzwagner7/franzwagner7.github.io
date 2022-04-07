b20390182bcbeaac86f6be0f281f71918d0f9ba8
Franz Wagner <franz.wagner@student.uibk.ac.at>
2022-04-07 15:42:44 +0200

    koordinate durch variablen ersetzt

--- nz/main.js
+++ nz/main.js
@@ -1,11 +1,14 @@
 // Neuseelandreise Skript we
 
-var map = L.map('map').setView([-39.195, 175.620], 5);
+let lat = -39.195
+let lng = 175.620
+let zoom = 5
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-39.195, 175.620]).addTo(map)
+L.marker([lat, lng]).addTo(map)
     .bindPopup('Tongariro Nationalpark')
     .openPopup();
