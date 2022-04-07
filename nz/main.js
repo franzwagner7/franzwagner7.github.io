445050899f38186b7eef3bdebb5f540d32457ddf
Franz Wagner <franz.wagner@student.uibk.ac.at>
2022-04-07 17:43:01 +0200

    Popups für Hütten eingerichtet

--- nz/main.js
+++ nz/main.js
@@ -6,7 +6,7 @@ let zoom = 5;
 
 let coords = [-39.195, 175.620];
 
-let pop =`
+/* let pop =`
     <h3>${ETAPPEN[0].titel} (Etappe ${ETAPPEN[0].nr})</h3>
     <ul>
         <li>geogr. Länge: ${ETAPPEN[0].lng}</li>
@@ -14,7 +14,7 @@ let pop =`
         <li><a href=${ETAPPEN[0].wikipedia}>Wikipedia</a></li>
         <li><a href=${ETAPPEN[0].github}>github</a></li>
     </ul>
-    `;
+    `; */
 
 /* console.log(coords[0]);
 console.log(coords.length);
@@ -27,9 +27,9 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([lat, lng]).addTo(map)
+/* L.marker([lat, lng]).addTo(map)
     .bindPopup(pop)
-    .openPopup();
+    .openPopup(); */
 
 for (let etappe of ETAPPEN) {
     let pop =`
@@ -38,8 +38,15 @@ for (let etappe of ETAPPEN) {
         <li>geogr. Länge: ${etappe.lng}</li>
         <li>geogr. Breite: ${etappe.lat}</li>
         <li><a href=${etappe.wikipedia}>Wikipedia</a></li>
-        <li><a href=${etappe.github}>github</a></li>
+        <li><a href="$https://${etappe.github}.github.io/nz">github</a></li>
     </ul>
     `;
     L.marker([etappe.lat, etappe.lng]).addTo(map).bindPopup(pop);
+}
+
+for (let hut of HUTS) {
+    let pop =`
+        <h3>${hut.name}</h3>
+    `;
+    L.circleMarker([hut.lat, hut.lng]).addTo(map).bindPopup(pop);
 }
