745fe9221c040fd8ba51d5dd5e9b07fc5adc69cc
Franz Wagner <franz.wagner@student.uibk.ac.at>
2022-04-07 17:17:27 +0200

    alle popups hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -1,25 +1,25 @@
 // Neuseelandreise Skript we
 
-let lat = -39.195;
-let lng = 175.620;
+let lat = -36.832782847837784;
+let lng = 175.5833761917986;
 let zoom = 5;
 
 let coords = [-39.195, 175.620];
 
 let pop =`
-    <h3>Tongariro Nationalpark</h3>
+    <h3>${ETAPPEN[0].titel} (Etappe ${ETAPPEN[0].nr})</h3>
     <ul>
         <li>geogr. Länge: ${ETAPPEN[0].lng}</li>
         <li>geogr. Breite: ${ETAPPEN[0].lat}</li>
         <li><a href=${ETAPPEN[0].wikipedia}>Wikipedia</a></li>
         <li><a href=${ETAPPEN[0].github}>github</a></li>
     </ul>
-    `
+    `;
 
-console.log(coords[0]);
+/* console.log(coords[0]);
 console.log(coords.length);
 console.log(`text ${lat}`);
-console.log(ETAPPEN[0])
+console.log(ETAPPEN[0]) */
 
 let map = L.map('map').setView([lat, lng], zoom);
 
@@ -30,3 +30,16 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([lat, lng]).addTo(map)
     .bindPopup(pop)
     .openPopup();
+
+for (let etappe of ETAPPEN) {
+    let pop =`
+    <h3>${etappe.titel} (Etappe ${etappe.nr})</h3>
+    <ul>
+        <li>geogr. Länge: ${etappe.lng}</li>
+        <li>geogr. Breite: ${etappe.lat}</li>
+        <li><a href=${etappe.wikipedia}>Wikipedia</a></li>
+        <li><a href=${etappe.github}>github</a></li>
+    </ul>
+    `;
+    L.marker([etappe.lat, etappe.lng]).addTo(map).bindPopup(pop);
+}
